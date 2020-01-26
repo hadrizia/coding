495a974fc29b97f30f25fdecc6cf29337e63be0f
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-01-25 21:17:17 -0300

    Implementing merge-sort

--- code/sorting/insetion_sort.py
+++ code/sorting/insetion_sort.py
@@ -6,8 +6,17 @@ def insertion_sort(array):
       array[i + 1] = array[i]
       i -= 1
     array[i + 1] = key
-    print array
   return(array)
 
+def nonincreasing_insertion_sort(arr):
+  for j in range(1, len(arr)):
+    key = arr[j]
+    i = j - 1
+    while i >= 0 and arr[i] < key:
+      arr[i + 1] = arr[i]
+      i -= 1
+    arr[i + 1] = key
+  return arr
 
 print insertion_sort([5, 2, 4, 6, 1, 3])
+print nonincreasing_insertion_sort([5, 2, 4, 6, 1, 3])
