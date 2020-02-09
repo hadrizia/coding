73b8c27710aa8af50cb46f9322cf14f383bfe462
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-02-09 16:25:53 -0300

    Finishing first question from chapter 1

--- code/cracking-the-coding-interview/cap_1_strings_and_arrays/1.1.is-unique.py
+++ code/cracking-the-coding-interview/cap_1_strings_and_arrays/1.1.is-unique.py
@@ -12,17 +12,41 @@ def isUnique(string):
       return False
   return True
 
+'''
+  Assuming that N = len(string)
+    Time efficiency: O(N^2)
+    Memory efficiency: O(1)
+'''
+def isUniqueWithoutDataStructure1(string):
+  n = len(string)
+  for i in range(n - 1):
+    for j in range(i + 1, n):
+      if string[i] == string[j]:
+        return False
+  return True
+
+'''
+Assuming that N = len(string)
+  Time efficiency: O(N * log N + N) = O(N * logN)
+  Memory efficiency: O(N)
+'''
+def isUniqueWithoutDataStructure2(string):
+  string_sorted = sorted(string)
+  for i in range(len(string_sorted) - 1):
+    if string_sorted[i] == string_sorted[i + 1]:
+      return False
+  return True
 
 def tests():
   assert isUnique('Hadrizia') == False
-  assert isUnique('HadrizIA') == True
-  assert isUnique('HadRzi') == True
+  assert isUniqueWithoutDataStructure1('HadrizIA') == True
+  assert isUniqueWithoutDataStructure2('HadRzi') == True
   assert isUnique('HAdrizIa') == True
-  assert isUnique('Hadrize') == True
-  assert isUnique('Gabriely') == True
+  assert isUniqueWithoutDataStructure1('Hadrize') == True
+  assert isUniqueWithoutDataStructure2('Gabriely') == True
   assert isUnique('ANA') == False
-  assert isUnique('IsUnique') == True
-  assert isUnique('IsUnqe') == True
+  assert isUniqueWithoutDataStructure1('IsUnique') == True
+  assert isUniqueWithoutDataStructure2('IsUnqe') == True
   assert isUnique('Marie Jul') == True
 
 if __name__ == "__main__":
