2e114ba64ed9ba02b89885a96f3b6d9257ac35c8
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-02-10 19:42:01 -0300

    Beautifying answer

--- code/cracking-the-coding-interview/cap_1_strings_and_arrays/1.5.one-away.py
+++ code/cracking-the-coding-interview/cap_1_strings_and_arrays/1.5.one-away.py
@@ -1,57 +1,41 @@
-def awaysOfInsertionOrRemove(string1, string2):
-  away = 0
-  i = 0
-  j = 0
-  while i <= len(string1) - 1 and j <= len(string2) - 1 and away <= 1:
-    if string1[i] == string2[j]:
-      j += 1
-    else:
-      away += 1
-    i += 1
-  return away
-
-def awaysOfReplace(string1, string2):
-  away = 0
-  for i in xrange(len(string1)):
-    if string1[i] != string2[i]:
-      away += 1
-
-    if away > 1:
-      break
-  return away
-    
 '''
   Assuming that M = min(len(string1), len(string2)):
     Time efficiecy: O(M)
     Memory efficiecy: O(M)
 '''
-def checkAway(string1, string2):
+def check_away(string1, string2):
   away = 0
   len_string1 = len(string1)
   len_string2 = len(string2)
 
-  if len_string1 - len_string2 == 1:
-    away = awaysOfInsertionOrRemove(string1, string2)
+  if len_string1 - len_string2 == 0:
+    away = 0
+    minor = string1
+    major = string2
   
-  elif len_string2 - len_string1 == 1:  
-    away = awaysOfInsertionOrRemove(string2, string1)
-  
-  elif len_string1 == len_string2:
-    away = awaysOfReplace(string1, string2)
+  elif len_string1 - len_string2 > 0:  
+    away = 1
+    minor = string2
+    major = string1
   
   else:
-    return False
+    away = 1
+    minor = string1
+    major = string2
+  
+  for letter in minor:
+    if letter not in major:
+      away += 1
+      if away > 1:
+        return False
 
-  if away <= 1:
-    return True
-
-  return False
+  return True
 
 def tests():
-  assert checkAway('pale', 'ple') == True
-  assert checkAway('pale', 'pales') == True
-  assert checkAway('pale', 'bale') == True
-  assert checkAway('pale', 'bake') == False
+  assert check_away('pale', 'ple') == True
+  assert check_away('pale', 'pales') == True
+  assert check_away('pale', 'bale') == True
+  assert check_away('pale', 'bake') == False
 
 if __name__ == "__main__":
   tests()
