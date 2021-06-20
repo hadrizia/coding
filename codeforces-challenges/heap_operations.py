aeba1edc23e92620ca3003e8dd6203423462a550
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-06-20 14:13:37 -0300

    WIP-trying to solve 'heap operations' question

---
+++ codeforces-challenges/heap_operations.py
@@ -0,0 +1,65 @@
+'''
+  Link: https://codeforces.com/problemset/problem/681/C
+'''
+
+import heapq
+
+def print_operations(ops):
+  print(len(ops))
+  for o in ops:
+    print(o)
+
+
+def heap_operations(num_op):
+  heap = []
+  ops = []
+
+  for _ in range(num_op):
+    op = input()
+    if op == "removeMin":
+      if heap:
+        heapq.heappop(heap)
+      else:
+        ops.append("insert 0")
+      ops.append(op)
+        
+    else:
+      op, n = op.split(" ")
+      n = int(n)
+
+      if op == "insert":
+        heapq.heappush(heap, n)
+        ops.append(op + " " + str(n))
+
+      elif op == "getMin":
+        if not heap:
+          heapq.heappush(heap, n)
+          ops.append("insert " + str(n))
+
+        elif heap[0] == n:
+          pass
+
+        else:
+          while heap and heap[0] < n:
+            heapq.heappop(heap)
+            ops.append("removeMin")
+
+          if not heap or heap[0] < n:
+            heapq.heappush(heap, n)
+            ops.append("insert " + str(n))
+        ops.append(op + " " + str(n))
+
+  print_operations(ops)
+
+    
+def main():
+  while True:
+    try:
+      num = int(input())
+      heap_operations(num)
+    except EOFError: 
+      break
+
+
+if __name__ == "__main__":
+  main()
